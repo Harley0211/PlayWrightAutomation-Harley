1118b3989434fed952268a6663addd688c00ac41
Harley0211 <harleydavevalencia@gmail.com>
2026-05-30 13:21:41 +0800

    Learn on how to run on headed

--- playwright.config.js
+++ playwright.config.js
@@ -2,19 +2,19 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
+const config = ({
   testDir: "./tests",
+  timeout: 40 * 1000, //global timeout
+  expect: {
+    timeout: 40 * 1000, //assertion timout
+  },
+  reporter:'html',
   use: {
+    browserName: "chromium",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
   },
 });
+
+module.exports = config
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,23 @@
-const { test } = require("@playwright/test");
+// const { test } = require("@playwright/test");
 
-test("Browser Context Playwright Test", async ({ browser, page }) => {
-  //chrome - plugins/ cookies
-  //   const context = await browser.newContext();
-  //   const page = await context.newPage();
-  await page.goto("https://rahulshettyacademy.com/loginpagePractice/");
+// test("Browser Context Playwright Test", async ({ browser, page }) => {
+//   //chrome - plugins/ cookies
+//   //   const context = await browser.newContext();
+//   //   const page = await context.newPage();
+//   await page.goto("https://www.rahulshettyacademy.com/loginpagePractise/");
+// });
+
+// test.only("Page Playwright Test", async ({ page }) => {
+//   await page.goto("https://www.google.com/");
+// });
+
+
+const {test} = require('@playwright/test');
+
+test('browser playwright test', async ({browser, page})=>{
+  await page.goto('https//:www.fiber.t-mobile.com')
 });
 
-test("Page Playwright Test", async ({ page }) => {
-  page.goto("https//:rahulshettyacademy.com/loginepagePractice/");
-});
+test ('Page playwright test', async ({page})=>{
+  await page.goto("https//:www.google.com")
+})
